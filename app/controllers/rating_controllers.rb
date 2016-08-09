0d069fea7f4aabe4edca12b5403166de369ec480
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-08-09 12:22:29 -0700

    users can edit or delete ratings

--- app/controllers/rating_controllers.rb
+++ app/controllers/rating_controllers.rb
@@ -15,3 +15,24 @@ post '/rating/new' do
 	@rating = Rating.create(food: params[:food], family_friendly: params[:family_friendly], history: params[:history], sites: params[:sites],cost: params[:cost], hospitality: params[:hospitality], modernism: params[:modernism], user_id: @user, place_id: @place.id)
 	redirect "/users/#{@user}"	
 end	
+
+delete '/rating/:id' do
+	@user = current_user.id
+	@rating = Rating.find(params[:id])
+	@rating.destroy
+	redirect "/users/#{@user}"
+end
+
+get '/rating/:id/edit' do
+	@rating = Rating.find(params[:id])
+	p @rating
+	erb :'ratings/edit'
+end
+
+put '/rating/:id' do
+	@user = current_user.id	
+	@rating = Rating.find(params[:id])
+	@rating.update(food: params[:food], family_friendly: params[:family_friendly], history: params[:history], sites: params[:sites],cost: params[:cost], hospitality: params[:hospitality], modernism: params[:modernism])
+
+	redirect "/users/#{@user}"
+end	
